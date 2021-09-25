704447e85cccfcbbefa80f3043a036be84825196
Jake Stone <Stonedev12@gmail.com>
2021-09-25 14:21:33 -0700

    Moving on.

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+
 /* Data */
 var data = {
   events: [],
   editing: null,
-  nextEntryId: 1
+  nextEntryId: 1,
+  view: ''
 };
 
 var savedData;
@@ -10,15 +12,46 @@ var savedDataJSON = localStorage.getItem('javascript-local-storage');
 /* getting previous entry ID and incrementing */
 if (savedDataJSON !== null) {
   savedData = JSON.parse(savedDataJSON);
+  data.events = savedData.entries;
+  data.view = savedData.view;
   data.nextEntryId = savedData.nextEntryId;
 }
 
 /* main */
+function showEventForm() {
+  data.view = 'event-form';
+  eventForm.style.visibility = 'visible';
+
+  var saveInputJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', saveInputJSON);
+}
+
+function showMain() {
+  data.view = 'add-event';
+  eventForm.style.visibility = 'hidden';
+
+  var saveInputJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', saveInputJSON);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (data.view === 'event-form') {
+    showEventForm();
+  } else {
+    showMain();
+  }
+});
+
 var addEvent = document.getElementById('addEntry');
 var eventForm = document.getElementById('invisible-row');
 
 addEvent.addEventListener('click', function (event) {
-  eventForm.style.visibility = 'visible';
+  showEventForm();
+});
+
+var exitForm = document.getElementById('exit');
+exitForm.addEventListener('click', function (event) {
+  showMain();
 });
 
 var form = document.getElementById('formId');
@@ -48,6 +81,15 @@ function handleSubmit(event) {
 
   eventForm.style.visibility = 'hidden';
   form.reset();
+  showMain();
 }
 
 form.addEventListener('submit', handleSubmit);
+
+function addEvent(event) {
+  var dayEventTitle = document.createElement('h2');
+  dayEventTitle.textContent = 'Scheduled Events for ' + form.day.value;
+
+  var dayEventTable = document.createElement('table');
+
+}
